a16a06ca47a3e66f3fdb6d4f8226118d9ddcff8e
couchjanus <couchjanus@gmail.com>
2021-12-09 21:34:15 +0200

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 # comment
-hell = "Hello World"
-print(hell)
-a = 22
-b = 2
+# hell = "Hello World"
+# print(hell)
+# a = 22
+# b = 2
 
-c = a ** b
+# c = a ** b
+# print()
+# print(c)
+
+a = input("Enter a = ")
+b = input("Enter b = ")
+o = '-'
+if o == '+':
+    c = int(a) + int(b)
+elif o == '-':
+    c = int(a) - int(b)
+elif o == '*':
+    c = int(a) * int(b)
+
 print()
-print(c)
-
+print("a " + o + " b = ", c)
